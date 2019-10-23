1085c0bd7e6836b93ff90eea1619c692fa9bfe34
masa5555 <masa57887@gmail.com>
2019-10-23 19:48:52 +0900

    add jquery for btn

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function selectSize() {
-  let a = 1;
-}
+//クリックされた要素にclass追加,削除,他に存在するselected class削除
+$( function() {
+  
+  $(".size").click( function() {
+    $(".size").removeClass("selected").addClass( "notSelected" );
+    var clickIndex = $(".size").index(this);
+    $(".size").eq(clickIndex).removeClass("notSelected").addClass( "selected" );
+  })
+
+  $(".type").click( function() {
+    $(".type").removeClass("selected").addClass( "notSelected" );
+    var clickIndex = $(".type").index(this);
+    $(".type").eq(clickIndex).removeClass("notSelected").addClass( "selected" );
+  })
+})
 
 function copyToClipboard() {
   var copyTarget = document.getElementById("copyTarget");
