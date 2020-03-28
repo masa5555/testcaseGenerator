0f6bf27f5d24284e41997b1822f4af00fcc4627a
masa5555 <masa57887@gmail.com>
2020-03-29 05:45:04 +0900

    refactored

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-function copyToClipboard() {
+//下のテストケース生成で使う
+function copyToClipboard(){
   let copyTarget = document.getElementById("copyTarget");
   copyTarget.select();
   document.execCommand("Copy");
 }
-function randN( n ){
-  return Math.floor( Math.random()*n);
+
+function randN(n){
+  return Math.floor( Math.random()*n );
 }
+
 function randAZ(){
-  let num = randN( 26 );
+  let num = randN(26);
   let az = "abcdefghijklmnopqrstuvwxyz";
   return az.charAt(num);
 }
-//クリックされた要素にclass追加,削除,他に存在するselected class削除
+
+//jQuery
 $( function() {
+  //sizeボタンを押すと呼び出される
   $( ".size" ).click( function() {
     $( ".size" ).removeClass( "selected" ).addClass( "notSelected" );
     let clickIndex = $(" .size" ).index(this);
     $( ".size" ).eq( clickIndex ).removeClass( "notSelected" ).addClass( "selected" );
   })
 
+  //typeボタンを押すと呼び出される
   $( ".type" ).click( function() {
     $( ".type" ).removeClass( "selected" ).addClass( "notSelected" );
     var clickIndex = $( ".type" ).index( this );
     $( ".type" ).eq( clickIndex ).removeClass( "notSelected" ).addClass( "selected" );
   })
   $(".btn" ).click( function(){
+    // 2種類のボタンが選択されている
     if( $( ".size" ).hasClass( "selected" ) && $( ".type" ).hasClass( "selected" ) ){
+      //input
       let size = $( ".size.selected" ).text();
       let type = $( ".type.selected" ).text();
+
+      //first line of test case
       let testStr = size + "\n";
-      Number( size );
+      Number( size ); // type: string => number
       if( type == "sequence" ){
         for(let i = 0; i < size; i++){
           testStr += randN( size ).toString();
@@ -46,6 +56,7 @@ $( function() {
       }
       $( "textarea" ).text( testStr );
       copyToClipboard();
+
     }else{
       alert( "選択されていません" );
     }
